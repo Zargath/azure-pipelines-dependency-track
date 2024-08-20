e0a450c7d96897038dcce8fc520cb78817564867
Edouard Shaar <zarthia@gmail.com>
2024-08-20 10:29:37 -0400

    Added tests

--- UploadBOM/tests/thresholdExpert.test.js
+++ UploadBOM/tests/thresholdExpert.test.js
@@ -10,18 +10,35 @@ test('Should not evaluate threshold if all thresholds are less than zero.', () =
 });
 
 test('Should evaluate threshold if any threshold not less than zero.', () => {
-    let expert = new ThresholdExpert(-1, 0, -1, -1, -1, -1, -1, -1, -1);
+    const thresholds = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
+    for (let i = 0; i < thresholds.length; i++) {
+        thresholds[i] = 0
 
-    let expected = true;
-    let actual = expert.areThresholdsValidated();
-    
-    expect(actual).toBe(expected);
+        let expert = new ThresholdExpert(...thresholds);
+
+        let expected = true;
+        let actual = expert.areThresholdsValidated();
+        
+        expect(actual).toBe(expected);
+
+        thresholds[i] = -1
+    }
 });
 
-test('Should not throw error when critical count is 1 and critical threshold is less than zero.', () => {
-    let criticalThreshold = -1;
-    let expert = new ThresholdExpert(criticalThreshold, -1, -1, -1, -1, -1, -1, -1, -1);
-    let metrics = { critical: 1 }
+test('Should not throw error when any vulnerability count is 1 and the threshold is less than zero.', () => {
+    const metrics = { 
+        critical: 1, 
+        high: 1, 
+        medium: 1, 
+        low: 1, 
+        unassigned: 1, 
+        policyViolationsFail: 1, 
+        policyViolationsWarn: 1, 
+        policyViolationsInfo: 1, 
+        policyViolationsTotal: 1 
+    }
+
+    let expert = new ThresholdExpert(-1, -1, -1, -1, -1, -1, -1, -1, -1);
     
     expect(() => { expert.validateThresholds(metrics) }).not.toThrow();
 });
