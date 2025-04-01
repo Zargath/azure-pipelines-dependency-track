c20621c7beb4b04428b6d683515f50260aa7d248
Edouard Shaar <zarthia@gmail.com>
2025-03-31 22:25:28 -0400

    Updated dtrack client tests

--- UploadBOM/src/__tests__/dtrackClient.test.js
+++ UploadBOM/src/__tests__/dtrackClient.test.js
@@ -1,48 +1,49 @@
 import DTrackClient from '../dtrackClient';
 import request from 'request';
 
-// Mock the `request` function
 jest.mock('request', () => jest.fn());
 
 describe('DTrackClient', () => {
-  let client;
 
   beforeEach(() => {
-    // Create a new instance of DTrackClient before each test
-    client = new DTrackClient('http://localhost:8080', 'test-api-key', null);
-
-    // Clear all mock calls before each test
     jest.clearAllMocks();
   });
 
   it('should get project info successfully', async () => {
-    // Arrange: Mock the request function to simulate a successful response
+    // Arrange
+    const apiKey = 'test-api-key';
+    const projectId = '123e4567-e89b-12d3-a456-426614174000';
+    const client = new DTrackClient('http://localhost:8080', apiKey);
     request.mockImplementation((url, options, callback) => {
-      callback(null, { statusCode: 200, body: { uuid: '123e4567-e89b-12d3-a456-426614174000' }});
+      callback(null, { statusCode: 200, body: { uuid: projectId } });
     });
 
-    // Act: Call the method
-    const result = await client.getProjectInfo('123e4567-e89b-12d3-a456-426614174000');
+    // Act
+    const result = await client.getProjectInfo(projectId);
 
-    // Assert: Verify the result and that the request was called correctly
-    expect(result).toEqual({ uuid: '123e4567-e89b-12d3-a456-426614174000' });
+    // Assert
+    expect(result).toEqual({ uuid: projectId });
     expect(request).toHaveBeenCalledWith(
-      '/api/v1/project/123e4567-e89b-12d3-a456-426614174000',
+      `/api/v1/project/${projectId}`,
       expect.objectContaining({
         method: 'GET',
-        headers: { 'X-API-Key': 'test-api-key' }
+        headers: { 'X-API-Key': apiKey }
       }),
       expect.any(Function)
     );
   });
 
   it('should handle errors when getting project info', async () => {
-    // Arrange: Mock the request function to simulate an error
+    // Arrange
+    const client = new DTrackClient('http://localhost:8080', 'test-api-key');
     request.mockImplementation((url, options, callback) => {
       callback('Not Found', { statusCode: 404 });
     });
 
-    // Act & Assert: Verify that the method throws an error
-    await expect(client.getProjectInfo('123e4567-e89b-12d3-a456-426614174000')).rejects.toEqual({error: 'Not Found', response: { statusCode: 404 }});
+    // Act
+    const promise = client.getProjectInfo('123e4567-e89b-12d3-a456-426614174000');
+
+    // Assert
+    await expect(promise).rejects.toEqual({ error: 'Not Found', response: { statusCode: 404 } });
   });
 });
