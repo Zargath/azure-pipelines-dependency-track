10aa172344f8944c85291b61cd483078e90812e4
Edouard Shaar <edouard.shaar@eshaar.me>
2025-10-18 12:10:38 -0400

    Fix error messages util and erros in tests.

--- UploadBOM/src/utils.js
+++ UploadBOM/src/utils.js
@@ -4,19 +4,21 @@ class Utils {
     }
     
     static getErrorMessage(err){
+        if (err.response) {
+            return `${err.response.status} - ${err.response.statusText}`;
+        }
+        
         if (err.error) {
             let errorMsg;
             try {
-            errorMsg = JSON.stringify(err.error);
+                errorMsg = JSON.stringify(err.error);
             }
             catch {
-            errorMsg = err.error;
+                errorMsg = err.error;
             }
 
             return `${errorMsg}`;
-        } else if (err.response) {
-            return `${err.response.statusCode} - ${err.response.statusMessage}`;
-        }
+        } 
 
         return `${err}`;
     }
